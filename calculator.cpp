109a0f77cb7d3937a314b3e46c88c36679ab1279
CodeWarrior21928 <112792433+CodeWarrior21928@users.noreply.github.com>
2024-10-11 17:34:53 +0600

    addition, division

--- calculator.cpp
+++ calculator.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int addition(int a, int b)
 {
-
+    return a+b;
 }
 int division(int a, int b)
 {
-
+    return a/b;
 }
 int multiplication(int a,int b)
 {
@@ -14,11 +14,12 @@ int multiplication(int a,int b)
 }
 int subtraction(int a, int b)
 {
-    
+
 }
 int main()
 {
     int a,b;
     cin>>a>>b;
-    
+    cout<<addition(a,b)<<endl;
+    cout<<division(a,b)<<endl; 
 }
