2cdedca7dc6d2199268a4e07a61c34969f705135
Faruq Ahammad <67782935+FaruqEmsl@users.noreply.github.com>
2024-10-11 17:31:00 +0600

    mul, sub

--- calculator.cpp
+++ calculator.cpp
@@ -10,15 +10,17 @@ int division(int a, int b)
 }
 int multiplication(int a,int b)
 {
-
+    return a*b;
 }
 int subtraction(int a, int b)
 {
-    
+    return a-b;
 }
 int main()
 {
     int a,b;
     cin>>a>>b;
     
+    cout<<multiplication(a,b) <<endl;
+    cout<<subtraction(a,b) <<endl;
 }
